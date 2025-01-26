48b55d394e4c1ff5ad33cfd6da8b8d0498c42413
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-01-26 20:36:34 +0530

    Time: 0 ms (100%), Space: 8.9 MB (97.15%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    bool isDigit(char ch){
-        int ans = false;
-        if( ch >= '0' && ch <= '9')
-            ans = true;
-        return ans;
-    }
-    bool isSpace(char ch){
-        int ans = false;
-        if( ch == ' ' || ch == '\n')
-            ans = true;
-        return ans;
-    }
+    
+    
     
     int myAtoi(string s){
         long result = 0;
         int i = 0;
         int sign = 1;
         
-        while (i < s.length() && isSpace(s[i]))
+        while (i < s.length() && s[i]==' ')
             i++;
         
         if (i < s.length() && (s[i] == '+' || s[i] == '-')){
@@ -28,7 +18,7 @@ public:
                 sign = 1;
             i++;
         }
-        while (i < s.length() && isDigit(s[i])) {
+        while (i < s.length() && isdigit(s[i])) {
             result = result * 10 + (s[i] - '0');
             
             if (result * sign > INT_MAX) {
@@ -38,8 +28,6 @@ public:
             }
             i++;
         }
-        return result * sign;
-        
-    }
-           
+        return result * sign;    
+    }     
 };
