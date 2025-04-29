b51a8a72227d46cb202a565a119da49c63f4a966
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-04-29 23:31:00 +0530

    Time: 0 ms (100%), Space: 97.3 MB (87.08%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int max_prof = 0;
+        int profit = 0;
         int min_price = INT_MAX;
-        int profit;
+
         for(int i = 0; i < prices.size(); i++){
             min_price = min(min_price, prices[i]);
-            profit = prices[i] - min_price;
-            max_prof = max(max_prof, profit);
+            profit = max(profit, prices[i] - min_price);
         }
-        return max_prof;
+
+        return profit;
+
     }
 };
