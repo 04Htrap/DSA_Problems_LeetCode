5c8db3f09dd80865019ae0c2a77fc8c5d8f27c40
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-06-13 13:52:09 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.46%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int ans = start ^ goal;
-        return __builtin_popcount(ans);
+        int helper = start ^ goal;
+        int cnt = 0;
+        while (helper > 0){
+            cnt = cnt + (helper & 1); // Checks if helper number is odd and if yes it ans with 1 and gives 1 in return
+            helper = helper >> 1; // To do n/2
+        }
+
+        if(helper == 1) cnt++;
+
+        return cnt;
     }
 };
