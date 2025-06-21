4028c61e033d2a7dfb4d33e029169f7e3592035d
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-06-21 23:11:33 +0530

    Time: 0 ms (100%), Space: 35.4 MB (61.01%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -11,10 +11,7 @@
  */
 class Solution {
 public:
-    TreeNode* searchBST(TreeNode* root, int val) {
-        if(root == nullptr)
-            return root;
-        
+    TreeNode* searchBST(TreeNode* root, int val) {        
         while(root != nullptr && root->val != val){
            root = val < root-> val ? root->left : root->right; 
         }
