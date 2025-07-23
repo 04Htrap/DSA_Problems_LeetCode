3779d4137b2d69c6d2b238c7efc03ef394748f35
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-07-23 14:52:07 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,30 +6,22 @@ public:
         while (low <= high){
             int mid = low + (high - low) / 2;
             
-            if (nums[mid] == target){
-                return mid;
-            }
-            
-            // Check if left of mid is sorted
-            if ( nums[low] <= nums[mid] ){
-                if( nums[low] <= target && target <= nums[mid] ){
+            if(nums[mid] == target) return mid;
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && nums[mid] >= target){
                     high = mid - 1;
                 }
-                else{
-                    low = mid + 1;
-                }
+                else low = mid + 1;
             }
-            
-            // Right of mid is sorted
             else{
-                if( nums[mid] <= target && target <= nums[high] ){
+                if(nums[mid] <= target && nums[high] >= target){
                     low = mid + 1;
                 }
-                else{
-                    high = mid -1;
-                }
-            }   
+                else high = mid - 1;
+            }
         }
+
         return -1;
+
     }
 };
