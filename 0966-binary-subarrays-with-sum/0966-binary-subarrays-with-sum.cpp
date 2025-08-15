487f5316a502c3d89bc52f57bfba0b0da92a67a5
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-08-15 13:39:18 +0530

    Time: 3 ms (54.31%), Space: 32.5 MB (97.46%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return atMost(nums, goal) - atMost(nums, goal - 1);
-    }
-
-    int atMost(vector<int>& nums, int goal) {
+    int countSub(vector<int>& nums, int goal) {
         if (goal < 0) return 0;
 
-        int left = 0, sum = 0, subarray_count = 0;
+        int left = 0, right = 0, sum = 0, sub_count = 0;
 
-        for (int right = 0; right < nums.size(); right++) {
+        while (right < nums.size()) {
             sum += nums[right];
 
             while (sum > goal) {
@@ -17,9 +13,13 @@ public:
                 left++;
             }
 
-            subarray_count += (right - left + 1);
+            sub_count += (right - left + 1);
+            right++;
         }
 
-        return subarray_count;
+        return sub_count;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return countSub(nums, goal) - countSub(nums, goal - 1);
     }
 };
