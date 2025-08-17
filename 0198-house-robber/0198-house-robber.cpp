13fbfd692dd643ed3ec2407d2704274438dcdb7b
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-08-17 15:07:53 +0530

    Time: 0 ms (100%), Space: 10.1 MB (80.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,9 +5,8 @@ public:
         int prev = nums[0];
         int prev2 = 0;
 
-        for(int i = 0; i < n; i++) {
-            int take = nums[i];
-            if(i > 1) take += prev2;
+        for(int i = 1; i < n; i++) {
+            int take = nums[i] + prev2;
             int not_take = 0 + prev;
 
             int curr = max(take, not_take);
