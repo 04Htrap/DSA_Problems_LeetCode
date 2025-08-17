dcc78e3f04ae2710fb5f61cf66ee55c5a54d6794
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-08-17 19:51:40 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.88%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    /* TABULATION */
+    int countWays(int m, int n, vector<vector<int>>& dp) {
+        for(int i  = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0){
+                    dp[0][0] = 1;
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+
+                if(i > 0)
+                    up = dp[i - 1][j];
+                if(j > 0)
+                    left = dp[i][j - 1];
+
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m - 1][n - 1];
+
+    }
+    /* MEMOIZATION
+    int countWays(int i, int j, vector<vector<int>>& dp) {
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = countWays(i - 1, j, dp);
+        int left = countWays(i, j - 1, dp);
+
+        return up + left;
+    }
+    */
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        return countWays(m, n, dp);
+    }
+
+
+};
