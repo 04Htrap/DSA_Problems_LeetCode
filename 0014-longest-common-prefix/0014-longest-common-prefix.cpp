0c46508b656c4a75ea430ea6b45bee4cd7801933
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-09-01 13:02:14 +0530

    Time: 0 ms (100%), Space: 11.7 MB (94.98%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+
+        string pref = strs[0];
+        for(int i = 0; i < strs.size(); i++) {
+            while(strs[i].find(pref) != 0) {
+                pref.pop_back();
+            }
+        }
+        return pref;
+    }
+};
