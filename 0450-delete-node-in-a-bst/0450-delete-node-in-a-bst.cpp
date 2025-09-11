26bb8904a535601899eccafb2388db1124008b1c
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-09-11 15:25:04 +0530

    Time: 0 ms (100%), Space: 34.2 MB (94.16%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,50 +11,50 @@
  */
 class Solution {
 public:
-    TreeNode* findMin(TreeNode* root){
-        while(root->left){
-            root = root->left;
-        }
-        return root;
-    }
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root == nullptr)
-            return nullptr;
-        
-        if(key < root->val){
+        if(root == nullptr) return root;
+
+        if(key < root->val) {
             root->left = deleteNode(root->left, key);
         }
-        else if(key > root->val){
+        else if(key > root->val) {
             root->right = deleteNode(root->right, key);
         }
+        //Now the node is found so we start deleting in 3 ways: 0, 1, 2 Children
         else{
-            //0 Child
-            if(root->left == nullptr && root->right == nullptr){
+            //Case with 0 child
+            if(root->left == nullptr && root->right == nullptr) {
                 delete root;
                 return nullptr;
             }
 
-            //1Child
-            if(root->left != nullptr && root-> right == nullptr){
+            //Case with 1 child on left
+            if(root->left != nullptr && root->right == nullptr) {
                 TreeNode* temp = root->left;
                 delete root;
                 return temp;
             }
 
-            if(root->left == nullptr && root-> right != nullptr){
+            //Case with 1 child on right
+            if(root->left == nullptr && root->right != nullptr) {
                 TreeNode* temp = root->right;
                 delete root;
                 return temp;
             }
 
-            //2 Child
+            //Final case with 2 children
             if(root->left != nullptr && root-> right != nullptr){
                 int mini = findMin(root->right)->val;
                 root->val = mini;
                 root->right = deleteNode(root->right, mini);
                 return root;
             }
-            
+        }
+        return root;
+    }
+    TreeNode* findMin(TreeNode* root){
+        while(root->left){
+            root = root->left;
         }
         return root;
     }
