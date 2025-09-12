e272c8e58a2097bca377244f6b4c25fa4e989c08
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-09-13 00:40:33 +0530

    Time: 14 ms (21.08%), Space: 19.4 MB (13.62%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+        for (char ch : s) {
+            if (vowels.count(ch)) {
+                return true; 
+            }
+        }
+        return false;
+    }
+};
