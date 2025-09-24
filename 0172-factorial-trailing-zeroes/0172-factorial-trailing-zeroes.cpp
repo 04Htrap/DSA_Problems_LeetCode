a2c75851718ce83a3fe027521cb20cce62d0f81c
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-09-24 12:49:07 +0530

    Time: 0 ms (100%), Space: 8 MB (86.02%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int trailingZeroes(int n) {
         int cnt_multiple = 0;
-        int power = 1;
 
         while(n > 0) {
             n = n / 5;
-            power++;
             cnt_multiple += n;
         }
         return cnt_multiple;
