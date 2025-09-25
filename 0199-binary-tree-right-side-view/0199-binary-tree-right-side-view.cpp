91eb6d186a776367b02cc5fe44b90e302bea609a
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-09-25 12:04:30 +0530

    Time: 0 ms (100%), Space: 15 MB (60.92%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -23,15 +23,13 @@ public:
             for(int i = 0; i < size; i++) {
                 TreeNode* temp = q.front();
                 q.pop();
-                if(i == size - 1) {
-                    res.push_back(temp->val);
-                }
+
+                if(i == size - 1) res.push_back(temp->val);
 
                 if(temp->left) q.push(temp->left);
                 if(temp->right) q.push(temp->right);
             }
         }
-
         return res;
     }
 };
