9da0cd2d52127a5c56ba433283ab09939bfa5613
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-09-26 20:57:50 +0530

    Time: 107 ms (18.16%), Space: 83.9 MB (19.41%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<vector<int>> res;
-        priority_queue<pair<int, vector<int>>> pq; //Dist & points sorted by dist
+        priority_queue<pair<int, vector<int>>> pq;
 
         for(auto point : points) {
             int dist = point[0] * point[0] + point[1] * point[1];
             pq.push({dist, point});
 
-            if(pq.size() > k) pq.pop();
+            if(pq.size() > k) 
+                pq.pop();
         }
-        
+
         while(!pq.empty()) {
             res.push_back(pq.top().second);
             pq.pop();
