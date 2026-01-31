d28f7ff60d0bc77ca714da8745fb69f923985c5c
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-01-31 12:09:51 +0530

    Time: 3 ms (34.97%), Space: 77.2 MB (44.21%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -13,25 +13,29 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* ans = new ListNode();
         ListNode* temp = ans;
-
         int carry = 0;
-        while(l1 != nullptr || l2 != nullptr || carry != 0){
+        while(l1 != nullptr || l2 != nullptr || carry != 0) {
             int sum = 0;
-            if(l1 != nullptr){
-                sum = sum + l1 -> val;
+            if(l1 != nullptr) {
+                sum += l1 -> val;
                 l1 = l1 -> next;
             }
-            if(l2 != nullptr){
-                sum = sum + l2 -> val;
+
+            if(l2 != nullptr) {
+                sum += l2 -> val;
                 l2 = l2 -> next;
             }
-            sum = sum + carry;
+
+            sum += carry;
             carry = sum / 10;
 
             ListNode* node = new ListNode(sum % 10);
             temp -> next = node;
             temp = temp -> next;
         }
+
         return ans -> next;
     }
 };
+
+// Time Complexity O(max(N,M))
