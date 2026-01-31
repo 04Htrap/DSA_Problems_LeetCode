4dab1cc9418166cf2f0b285545a25a254a544826
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-01-31 15:05:34 +0530

    Time: 60 ms (36.98%), Space: 23.9 MB (79.78%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* dummy1 = headA;
+        ListNode* dummy2 = headB;
+
+        while(dummy1 != dummy2) {
+            if(dummy1 == nullptr) {
+                dummy1 = headB;
+            }
+            else dummy1 = dummy1 -> next;
+
+            if(dummy2 == nullptr) {
+                dummy2 = headA;
+            }
+            else dummy2 = dummy2 -> next;
+        }
+        
+        return dummy2;
+    }
+};
+
+//Time Complexity O(N + M)
