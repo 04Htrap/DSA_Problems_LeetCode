3603649003905837f6711fa726f0e34a99cd3c73
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-02-18 10:56:58 +0530

    Time: 0 ms (100%), Space: 11 MB (73.22%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,21 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+  //Time Complexity O(n) because we visit every node once
 class Solution {
 public:
-    void postorder(TreeNode* root, vector<int> &arr){
-        if(root == NULL) {
+    void postorder(TreeNode* root, vector<int> &ans){
+        if(root == nullptr) {
             return;
         }
-        postorder(root->left, arr);
-        postorder(root->right, arr);
-        arr.push_back(root-> val);        
+        postorder(root->left, ans);
+        postorder(root->right, ans);
+        ans.push_back(root-> val);        
     }
 
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> arr;
-        postorder(root, arr);
-        return arr;
+        vector<int> ans;
+        postorder(root, ans);
+        return ans;
     }
 
 };
