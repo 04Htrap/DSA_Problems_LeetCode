30089053910e1085b1b6d6c9b24e6ae25c2cfc0d
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-06-11 12:20:51 +0530

    Time: 0 ms (100%), Space: 118.1 MB (64%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,9 +8,6 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
- //TIME COMPLEXITY O(n)
- 
 class Solution {
 public:
     ListNode* reverseList(ListNode* node) {
@@ -23,26 +20,24 @@ public:
             prev = curr;
             curr = temp;
         }
-
         return prev;
-
     }
+
     bool isPalindrome(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
 
         while(fast != nullptr && fast -> next != nullptr) {
             slow = slow -> next;
-            fast = fast->next-> next;
+            fast = fast -> next -> next;
         }
 
-        ListNode* head1 = head; //First Half
-        ListNode* head2 = reverseList(slow); //Second Half
+        ListNode* head1 = head;
+        ListNode* head2 = reverseList(slow);
 
         while(head2 != nullptr) {
             if(head1 -> val != head2 -> val) {
                 return false;
-                break;
             }
             head1 = head1 -> next;
             head2 = head2 -> next;
