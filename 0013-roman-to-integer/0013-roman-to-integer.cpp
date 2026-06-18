69f38b25848a4853d53532018b58e0bf28cab69a
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-06-18 15:24:56 +0530

    Time: 15 ms (10%), Space: 13.4 MB (37.7%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
+    //TIME COMPLEXITY O(n)
     int romanToInt(string s) {
         unordered_map<char, int> roman = 
             {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
 
-        int total = 0;
-        for (int i = 0; i < s.size(); i++) {
-            int curr = roman[s[i]];
-            int next = 0;
-
-            if (i + 1 < s.size()) {
-                next = roman[s[i + 1]];
+        int res = 0;
+        for(int i = 0; i < s.size() - 1; i++) {
+            if(roman[s[i]] < roman[s[i + 1]]) {
+                res -= roman[s[i]];
             }
-
-            if (curr < next) {
-                total -= curr;
-            } else {
-                total += curr;
+            else {
+                res += roman[s[i]];
             }
         }
-        return total;
+
+        return res + roman[s.back()];
     }
 };
