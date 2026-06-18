8bec076e01b820d2f04b986c266525c7dcf111e8
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-06-18 13:30:32 +0530

    Time: 4 ms (22.39%), Space: 11.3 MB (22.98%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int left = 0, right = s.length() - 1;
-        string res="";
-        string temp = "";
+        vector<string> words;
+        string word = "";
 
-        while (left <= right && s[left] == ' ') left++;
-        while (right >= left && s[right] == ' ') right--;
-
-
-        while(left <= right) {
-            char c = s[left];
-            if(c != ' ') {
-                temp += c;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] != ' ') {
+                word += s[i];
             }
-            else if(temp != "") {
-                if(res != "") res = temp + " " + res;
-                else res = temp;
-                temp = "";
+            else if(!word.empty()) {
+                words.push_back(word);
+                word = "";
             }
-            left++;
         }
 
-        if(temp != "") {
-            if(res != "") res = temp + " " + res;
-            else res = temp;
+        if(!word.empty()) {
+            words.push_back(word);
+        }
+
+        reverse(words.begin(), words.end());
+        string res = "";
+        for(int i = 0; i < words.size(); i++) {
+            res += words[i];
+            if(i < words.size() - 1) {
+                res += " ";
+            }
         }
         return res;
     }
