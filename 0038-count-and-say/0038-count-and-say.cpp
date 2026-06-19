bb722d3615b895a13536520f453fada1935fdeb6
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-06-19 14:45:06 +0530

    Time: 4 ms (48.56%), Space: 9.8 MB (80.95%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    //TIME COMPLEXITY O(2^n)
+    //SPACE COMPLEXITY O(2^n)
+    string countAndSay(int n) {
+        string res = "1";
+
+        for(int i = 1; i < n; i++) {
+            string curr = "";
+            int cnt = 1;
+
+            for(int j = 1; j < res.size(); j++) {
+                if(res[j] == res[j - 1])
+                    cnt++;
+                else {
+                    curr += to_string(cnt) + res[j - 1];
+                    cnt = 1;
+                }
+            }
+            curr += to_string(cnt) + res.back();
+            res = curr;
+        }
+        return res;
+    }
+};
