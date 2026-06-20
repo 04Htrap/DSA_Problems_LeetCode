79858544ed78a1a60e554aa0eadb9cf29f06d706
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-06-21 02:24:13 +0530

    Time: 0 ms (100%), Space: 28.2 MB (27.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return nums[n/2];
+        int element = 0;
+        int count = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(count == 0) {
+                element = nums[i];
+                count++;
+            }
+            else if(nums[i] == element)
+                count++;
+            else
+                count--;
+        }
+        return element;
     }
 };
