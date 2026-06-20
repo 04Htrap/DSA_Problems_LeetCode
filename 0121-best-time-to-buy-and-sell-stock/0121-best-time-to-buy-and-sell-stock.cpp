b06fe47b17e64a74fa0e3c29241fbb6aad54ee41
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-06-20 13:12:00 +0530

    Time: 3 ms (28.31%), Space: 97.3 MB (87.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
+    //TIME COMPLEXITY O(n)
+    //SPACE COMPLEXITY O(1)
     int maxProfit(vector<int>& prices) {
         int profit = 0;
         int min_price = INT_MAX;
 
-        for(int i = 0; i < prices.size(); i++){
+        for(int i = 0; i < prices.size(); i++) {
             min_price = min(min_price, prices[i]);
             profit = max(profit, prices[i] - min_price);
         }
 
         return profit;
-
     }
 };
