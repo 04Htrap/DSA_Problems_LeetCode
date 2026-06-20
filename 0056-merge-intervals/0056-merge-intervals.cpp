5e080daaf3f872ee5f27ca286d5322805d9c6154
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-06-20 18:48:42 +0530

    Time: 4 ms (65.79%), Space: 23.8 MB (82.03%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,22 +1,22 @@
-//Time Complexity O(nlogn) // Because of sorting
 class Solution {
 public:
+    //TIME COMPLEXITY O(nlogn)
+    //SPACE COMPLEXITY O(n)
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> res;
         sort(intervals.begin(), intervals.end());
 
         vector<int> curr = intervals[0];
 
-        for(int i = 1; i < intervals.size(); i++) {
+        for(int i = 0; i < intervals.size(); i++) {
             if(curr[1] >= intervals[i][0]) {
-                curr[1] = max(curr[1], intervals[i][1]); 
+                curr[1] = max(curr[1], intervals[i][1]);
             }
             else {
                 res.push_back(curr);
                 curr = intervals[i];
             }
         }
-
         res.push_back(curr);
         return res;
     }
