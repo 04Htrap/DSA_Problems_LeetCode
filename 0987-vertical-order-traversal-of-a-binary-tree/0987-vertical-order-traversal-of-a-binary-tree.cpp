bf87b497fea320b7a7289ba9c4be080a861fffe8
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-07-01 14:40:51 +0530

    Time: 4 ms (22.45%), Space: 15.7 MB (77.81%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,55 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-//TIME COMPLEXITY O(nlogn) Each node visited and then each node inserted into the map
-//SPACE COMPLEXIXTY O(n) n for map, queue and output vector each
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        /*
-        SOLUTION LOGIC
-        //We break the tree into horizontal and vertical coordinates
-        //We make a map with key as col and the values have another map with the key as rows
-        //When the map is created and the entire tree is traversed we put them into a 2D array
-        */
-
+        //Create a map with col and then row as key and vector of nodes as values
         map<int, map<int, vector<int>>> mpp;
 
-        queue<pair<TreeNode*, pair<int,int>>> q;
+        queue<pair<TreeNode*, pair<int, int>>> q;
         q.push({root, {0,0}});
-
         while(!q.empty()) {
-            auto it = q.front();
+            auto curr = q.front();
             q.pop();
 
-            TreeNode* node = it.first;
-            int row = it.second.first;
-            int col = it.second.second;;
+            TreeNode* node = curr.first;
+            int row = curr.second.first;
+            int col = curr.second.second;
 
-            mpp[col][row].push_back(node->val);
-
-            if(node->left != nullptr)
-                q.push({node->left, {row + 1, col - 1}});
-
-            if(node->right != nullptr)
-                q.push({node->right, {row + 1, col + 1}});
+            mpp[col][row].push_back(node -> val);
+            if(node -> left) q.push({node -> left, {row + 1, col - 1}});
+            if(node -> right) q.push({node -> right, {row + 1, col + 1}});
         }
-        //map ready
 
         vector<vector<int>> ans;
-
         for(auto c : mpp) {
             vector<int> colVals;
             for(auto r : c.second) {
                 sort(r.second.begin(), r.second.end());
-                for(int val : r.second) {
-                    colVals.push_back(val);
-                }
+                for(int x : r.second)
+                    colVals.push_back(x);
             }
             ans.push_back(colVals);
         }
-
         return ans;
     }
 };
