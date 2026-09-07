a1f341d3bb7786a5430346b6950a2fa423a9fdff
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-09-07 13:17:48 +0530

    Time: 3 ms (67.25%), Space: 14.9 MB (44.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    //TIME COMPLEXITY O(n)
-    //SPACE COMPLEXITY O(n)
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+        unordered_map <int, int> mp;
 
         for(int i = 0; i < nums.size(); i++) {
-            int needed = target - nums[i];
-            if(mp.find(needed) != mp.end())
-                return{mp[needed], i};
+            int diff = target - nums[i];
+
+            if(mp.find(diff) != mp.end()) {
+                return {mp[diff], i};
+            }
             mp[nums[i]] = i;
         }
+
         return {};
     }
 };
+//TIME COmplexity O
